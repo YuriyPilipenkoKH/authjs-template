7b564872695e94141052c8ea306eefdd964c414c
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-11 16:14:27 +0200

    check for session

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { auth } from '../../../auth';
+import { redirect } from 'next/navigation';
 
-const AnminPage = () => {
+const AnminPage =  async() => {
+    const session = await auth();
+    if (!session) {
+      redirect('/login'); 
+    }
   return (
     <div>AnminPage</div>
   )
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { auth } from '../../../auth';
+import { redirect } from 'next/navigation';
 
-const DashboardPage = () => {
+const DashboardPage = async() => {
+  const session = await auth();
+  if (!session) {
+    redirect('/login'); 
+  }
   return (
     <div>DashboardPage</div>
   )
