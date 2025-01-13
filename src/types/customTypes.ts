a0f7833400783dcb05ec04566216a5009123167c
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-13 22:14:41 +0200

    applied Record utility from TypeScript

--- src/types/customTypes.ts
+++ src/types/customTypes.ts
@@ -1,5 +1,14 @@
 import { User } from '@prisma/client';
+import { z } from 'zod';
 
 export interface Userdetails {
   details: Pick<User, "image" | "name" | "role" >
 }
+
+export const taskStatusSchema = z.enum(['todo', 'inprogress', 'done']);
+export type TaskStatus = z.infer<typeof taskStatusSchema>;
+
+export type TaskSummary = Record<TaskStatus, Record<number, { count: number; name: string }>>;
+
+// Record<K, V>:
+// A TypeScript utility type that creates an object type with keys of type K and values of type V.
