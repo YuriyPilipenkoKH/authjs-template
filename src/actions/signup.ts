e4361709d77ecf14383090beb22873f594459fda
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-14 10:57:47 +0200

    unknown state:)

--- src/actions/signup.ts
+++ src/actions/signup.ts
@@ -2,10 +2,17 @@
 
 import { RegisterSchema } from "@/models/schemas"
 
-export async function signup(state, formData:FormData) {
+
+export async function signup(state:unknown, formData:FormData) {
+
   const validationResult = RegisterSchema.safeParse({
     name: formData.get('name'),
     email: formData.get('email'),
     password: formData.get('password'),
   })
+  if(!validationResult.success) {
+    return {
+      errors: validationResult.error.flatten().fieldErrors
+    }
+  }
 }
