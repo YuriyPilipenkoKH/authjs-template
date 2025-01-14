bdac205f857f55e8f3acb806546e767e3f8320bd
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-14 11:32:11 +0200

    recieved server validation

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 
 const RegisterPage = () => {
   return (
-    <div className='p-6 '>
+    <div className='p-6 flex flex-col gap-5 items-center justify-center'>
+      <h2>Welcome</h2>
       <SignUpForm/>
     </div>
   )
--- src/components/forms/SignUpForm.tsx
+++ src/components/forms/SignUpForm.tsx
@@ -7,16 +7,20 @@ import React, { useActionState } from 'react'
 
 
 const SignUpForm = () => {
-  const [ state, action ] = useActionState(signup,undefined)
+  const [ state, action , pending] = useActionState(signup,undefined)
+
   return (
     <form action={action}
-    className='flex flex-col gap-4 items-center justify-center'>
-      <input name='name' placeholder='name' className='input input-primary'/>
+    className='flex flex-col gap-4 items-center justify-center w-[400px]'>
+      <input name='name' placeholder='name' className='input input-primary w-full'/>
       {state?.errors?.name && <p>{state.errors.name}</p>}
-      <input name='email' placeholder='mail' className='input input-primary'/>
+      <input name='email' placeholder='mail' className='input input-primary w-full'/>
       {state?.errors?.email && <p>{state.errors.email}</p>}
-      <input name='password' placeholder='pass' className='input input-primary'/>
+      <input name='password' placeholder='pass' className='input input-primary w-full'/>
       {state?.errors?.password && <p>{state.errors.password}</p>}
+      <button className='btn btn-primary bg-green-900 w-full'>
+        {pending ? 'sending' : 'signup'}
+      </button>
     </form>
   )
 }
