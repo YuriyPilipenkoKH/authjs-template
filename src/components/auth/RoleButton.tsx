855eb3cc4bdffd3de919a5d466b491d78ad1f27d
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-16 01:09:38 +0200

    update RoleButton to use watchRole action and add button styling

--- src/components/auth/RoleButton.tsx
+++ src/components/auth/RoleButton.tsx
@@ -1,8 +1,17 @@
+import { watchRole } from '@/actions/watch-role'
+import { cn } from '@/lib/cn'
 import React from 'react'
 
 const RoleButton = () => {
   return (
-    <form action=""></form>
+          <form action={watchRole} >
+            <button
+              className={cn("btn btn-info logout-btn ", )}
+              // onClick={handleLogout}  
+              >
+                role
+            </button>
+          </form>
   )
 }
 
