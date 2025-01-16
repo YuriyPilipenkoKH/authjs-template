33fe293eef43b8d31978dbe6b97676dceea57d8d
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-16 10:58:51 +0200

    check for matching password

--- src/actions/signin.ts
+++ src/actions/signin.ts
@@ -1,5 +1,6 @@
 import { LoginClientSchema } from "@/models/schemas";
 import { prisma } from "../../prisma/prisma";
+import { compare } from "bcrypt-ts";
 
 export async function signin(formData:FormData) {
   const validationResult = await LoginClientSchema.safeParseAsync({
@@ -19,6 +20,18 @@ export async function signin(formData:FormData) {
       const existingUser = await prisma.user.findUnique({
       where: { email: validationResult.data.email  },
     });
+    if (!existingUser?.password) {
+      return { success: false, error: "Email is not registered." };
+    }
+    // Verify the password
+    const isMatched = await compare(validationResult.data.password, existingUser.password);
+
+    if (!isMatched) {
+      return { success: false, error: "Password did not match." };
+    }
+    // Exclude sensitive fields
+    
+
   } catch (error) {
     
   }
