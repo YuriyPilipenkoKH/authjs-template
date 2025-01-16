e64e8d073b0829d931c521d51fdca7509cca3967
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-16 10:47:16 +0200

    retreive values from formdata

--- src/actions/signin.ts
+++ src/actions/signin.ts
@@ -1,4 +1,9 @@
+import { LoginClientSchema } from "@/models/schemas";
 
 export async function signin(formData:FormData) {
-
+  const validationResult = await LoginClientSchema.safeParseAsync({
+   
+    email: formData.get('email'),
+    password: formData.get('password'),
+  })
 }
