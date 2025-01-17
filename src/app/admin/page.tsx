92e44f63ce5594297db4e6a91d69ba34944f0772
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-17 11:25:55 +0200

    updated dashboardpage

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { auth } from '../../../auth';
 import { redirect } from 'next/navigation';
 import { watchRole } from '@/actions/watch-role';
+import Link from 'next/link';
 
 
 const AnminPage =  async() => {
@@ -17,7 +18,9 @@ const AnminPage =  async() => {
     const userRole = await watchRole(formData); // Call server action
     if (userRole !== 'admin') redirect('/dashboard'); 
   return (
-    <div>AnminPage
+    <div className='flex flex-col gap-2 items-center justify-center'>
+      <h1>AnminPage</h1>
+      <Link href={'/dashboard'}>dashboard Page</Link>
       <div>name{' '}{session?.user?.name}</div>
       <div>role{' '}{userRole}</div>
       <div>photo{' '}{session?.user?.image}</div>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,18 +3,28 @@ import { auth } from '../../../auth';
 import { redirect } from 'next/navigation';
 import { LogoutButton } from '@/components/auth/LogoutButton';
 import RoleButton from '@/components/auth/RoleButton';
+import { watchRole } from '@/actions/watch-role';
+import Link from 'next/link';
 
 
 const DashboardPage = async() => {
   const session = await auth();
   console.log(session);
     if (!session) redirect('/login'); 
+    const email = session?.user?.email;
+    if (!email) redirect('/dashboard');
+
+    const formData = new FormData();
+    formData.append("email", email);
+     const userRole = await watchRole(formData)
+
   return (
     <div className='flex flex-col gap-2 items-center justify-center'>
       <h1>DashboardPage</h1>
-      <div>{session?.user?.name}</div>
-      <div>{session?.user?.role}</div>
-      <div>{session?.user?.image}</div>
+      {userRole === 'admin' && <Link href={'/admin'}>Admin Page</Link>}
+      <div>name{' '}{session?.user?.name}</div>
+      <div>role{' '}{userRole}</div>
+      <div>photo{' '}{session?.user?.image}</div>
      
       <LogoutButton username={session?.user?.name}/>
       <RoleButton email={session?.user?.email}/>
